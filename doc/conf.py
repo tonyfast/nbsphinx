83402c38fcb47c55e38808338e6ca2c591814d06
Matthias Geier <Matthias.Geier@gmail.com>
2016-02-27 13:36:07 +0100

    DOC: Switch to haiku theme

--- doc/conf.py
+++ doc/conf.py
@@ -106,6 +106,7 @@ except Exception:
 # -- Options for HTML output ----------------------------------------------
 
 html_title = project + ' version ' + release
+html_theme = 'haiku'
 
 # -- Options for LaTeX output ---------------------------------------------
 
