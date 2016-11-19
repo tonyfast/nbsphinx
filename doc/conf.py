ca0fdfb22d1f5d660c76aebce86f9e77d5f8b4a6
Matthias Geier <Matthias.Geier@gmail.com>
2016-11-19 10:09:12 +0100

    WIP enable save RST

--- doc/conf.py
+++ doc/conf.py
@@ -50,7 +50,7 @@ nbsphinx_execute_arguments = [
 #nbsphinx_responsive_width = '700px'
 
 # If True, the intermediate RST files are copied to the HTML output directory:
-#nbsphinx_save_rst = True
+nbsphinx_save_rst = True
 
 # This is processed by Jinja2 and inserted before each notebook
 nbsphinx_prolog = r"""
